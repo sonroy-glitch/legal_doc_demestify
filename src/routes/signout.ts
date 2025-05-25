b85f61021b8beb244e782a328a81420cd7d2f739
Sounak Roy <135100128+sonroy-glitch@users.noreply.github.com>
2025-05-26 01:58:56 +0530

    Delete src directory

--- src/routes/signout.ts
+++
@@ -1,8 +0,0 @@
-import {Request,Response} from 'express'
-
-export async function signout(req:Request,res:Response){
-    res.clearCookie('token', { path: 'http://localhost:5173' });
-    res.clearCookie('status', { path: 'http://localhost:5173' });
-    res.status(200).send("Signout Successful")
-
-}
