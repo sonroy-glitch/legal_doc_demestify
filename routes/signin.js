1d16f0e92c4e604ec2656daf4b72c5220625401e
Sounak Roy <135100128+sonroy-glitch@users.noreply.github.com>
2025-05-26 05:50:47 +0530

    Update signin.js

--- routes/signin.js
+++ routes/signin.js
@@ -46,8 +46,8 @@ function signin(req, res) {
                 console.log(search);
                 const token = jsonwebtoken_1.default.sign(JSON.stringify({ email: search.email }), jwtSecret);
                 const signedIn = jsonwebtoken_1.default.sign(JSON.stringify({ state: true }), jwtSecret);
-                res.cookie('status', signedIn,{ httpOnly: false, secure: true, sameSite: 'none', path: '/' });
-                res.cookie('token', token,{ httpOnly: false, secure: true, sameSite: 'none', path: '/' });
+                res.cookie('status', signedIn,{  domain:'https://finance-ai-summarizer.netlify.app'  secure: true, sameSite: 'none'});
+                res.cookie('token', token,{ domain:'https://finance-ai-summarizer.netlify.app' secure: true, sameSite: 'none' });
                 res.status(200).send('Login Success');
             }
         }
