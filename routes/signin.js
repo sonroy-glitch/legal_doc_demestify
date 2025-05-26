db32a004253d421079a432674b1c71dafcb740ea
Sounak Roy <135100128+sonroy-glitch@users.noreply.github.com>
2025-05-26 05:54:07 +0530

    Update signin.js

--- routes/signin.js
+++ routes/signin.js
@@ -46,8 +46,8 @@ function signin(req, res) {
                 console.log(search);
                 const token = jsonwebtoken_1.default.sign(JSON.stringify({ email: search.email }), jwtSecret);
                 const signedIn = jsonwebtoken_1.default.sign(JSON.stringify({ state: true }), jwtSecret);
-                res.cookie('status', signedIn,{  domain:'https://finance-ai-summarizer.netlify.app'  secure: true, sameSite: 'none'});
-                res.cookie('token', token,{ domain:'https://finance-ai-summarizer.netlify.app' secure: true, sameSite: 'none' });
+                res.cookie('status', signedIn,{   secure: true, sameSite: 'none'});
+                res.cookie('token', token,{  secure: true, sameSite: 'none' });
                 res.status(200).send('Login Success');
             }
         }
