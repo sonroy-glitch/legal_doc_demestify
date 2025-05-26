bc618c3c43105e69eaac5dd1c840e57b33f1b2a7
Sounak Roy <135100128+sonroy-glitch@users.noreply.github.com>
2025-05-26 08:11:47 +0530

    Update signout.js

--- routes/signout.js
+++ routes/signout.js
@@ -12,8 +12,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.signout = signout;
 function signout(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
-        res.clearCookie('token', { path: 'https://app.summarizer.shop/' });
-        res.clearCookie('status', { path: 'https://app.summarizer.shop/' });
+        res.clearCookie('token', { path: 'https://app.summarizer.shop' });
+        res.clearCookie('status', { path: 'https://app.summarizer.shop' });
         res.status(200).send("Signout Successful");
     });
 }
