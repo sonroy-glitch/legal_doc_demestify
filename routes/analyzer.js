dcf27ec02be3a3abebc26a1a51f58c3c2b77e6a0
Sounak Roy <135100128+sonroy-glitch@users.noreply.github.com>
2025-08-10 03:36:39 +0530

    Update analyzer.js

--- routes/analyzer.js
+++ routes/analyzer.js
@@ -18,14 +18,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.analyzer = analyzer;
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI(process.env.AI_KEY);
+const genAI = new GoogleGenerativeAI('AIzaSyCyRk4a7w7iJ5Iz2RjHYCDEAPu40_-iYQQ');
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 const { tavily } = require("@tavily/core");
 const client = tavily({ apiKey: process.env.TAVILY_KEY });
 
 const Redis = __importDefault(require("ioredis"));
-const redis = new Redis.default(process.env.REDIS_KEY);
+const redis = new Redis.default('rediss://default:Abp_AAIjcDE0MjQ5YjBiM2NkMzA0OWM4OWVlNTAyYTQ0YzA2YmQzZXAxMA@delicate-grub-47743.upstash.io:6379');
 
 const systemInstruction = {
     role: 'user',
